3bfb0ba84bf833cbcaaabff4a5df3d7ef9c9cbba
miaizo <miaizo@sina.com>
2018-07-30 01:18:51 +0800

    pos

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+      <left-nav></left-nav>
+      <div class="main">
+          <router-view></router-view>
+      </div>   
+  </div>
+</template>
+
+<script>
+import leftNav from "@/components/common/leftNav";
+export default {
+  name: "App",
+  components: {
+    leftNav
+  }
+};
+</script>
+
+<style>
+.main {
+  float: left;
+  width: 95%;
+  background-color: #ccc;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
